2fd10dce21d121949ed30bed49a6e02b0858072d
dladyj <dladyj@gmail.com>
2023-03-01 13:41:40 -0500

    bootstrapped tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@
 
 function App() {
   return (
-    <div className="App">
-    hello
-    </div>
+    //
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
   );
 }
 
